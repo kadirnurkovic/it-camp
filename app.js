6850842dc9d42f468d6b8abe250fb165f99ce356
kadirnurkovic <kadir.nurkovic0404@gmail.com>
2022-05-15 11:18:06 +0200

    js-for-loop

--- app.js
+++ app.js
@@ -59,28 +59,6 @@
         console.log(i);
     }
 }
-// const rec = "Sta ste radili danas";
-// let counter = 0;
-
-// for(let i = 0; i < rec.length; i++){
-//     if(rec[i] === " "){
-//         console.log(i);
-//         counter++;
-//     }
-// }
-// console.log(counter)
-
-// const string = "ovo je string"
-// let newString = "";
-
-// for (let i = 0; i < string.length; i++){
-//     const element = string[i];
-
-//     if (i === " ") {
-//        newString += element[]
-// }
- 
-// console.log(newString)
 
 
 
---
+++ jscript for loop/app.js
@@ -0,0 +1,23 @@
+const rec = "Sta ste radili danas";
+let counter = 0;
+
+for(let i = 0; i < rec.length; i++){
+    if(rec[i] === " "){
+        console.log(i);
+        counter++;
+    }
+}
+console.log(counter)
+
+// UKLONITI SPACE SA STRINGA
+
+const string = "ovo je string"
+let newString = "";
+
+for (let i = 0; i < string.length; i++){
+    const element = string[i];
+    if (element !== " ") {
+       newString += element
+}
+}
+console.log(newString)
